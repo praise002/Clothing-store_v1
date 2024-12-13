16cb58c278085275251791531e4313e1ccaa2ee8
Praise Idowu <ifeoluwapraise02@gmail.com>
2024-12-13 23:01:36 +0100

    Implemented star rating using js

--- apps/shop/forms.py
+++ apps/shop/forms.py
@@ -2,14 +2,25 @@ from django import forms
 
 from apps.shop.models import Review
 
+#required=False, doesn't print out value
 class ReviewForm(forms.ModelForm):
-    rating = forms.IntegerField(required=False)
+    rating = forms.IntegerField(
+        widget=forms.HiddenInput(attrs={"id": "star-rating"}),
+    ) 
     text = forms.CharField(
         max_length=255,
-        widget=forms.Textarea(attrs={"class": "form-control", "rows": "5", "placeholder": "Enter your review", 
-                                     "style": "resize: none"}),
+        widget=forms.Textarea(
+            attrs={
+                "class": "form-control",
+                "rows": "5",
+                "placeholder": "Enter your review",
+                "style": "resize: none",
+            }
+        ),
     )
 
     class Meta:
         model = Review
         fields = ["rating", "text"]
+
+# NOTE: WHEN REVIEW IS SUBMITTED FORM SHOULD BE CLOSED
